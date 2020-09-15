5c5add93d5417f8ce8cab72aff70baa39bd088fd
monstermaria <42701428+monstermaria@users.noreply.github.com>
2020-09-15 10:03:27 +0200

    Added a build and renamed it to docs

---
+++ docs/precache-manifest.37dc41f26a69f433f79f435f8113a17b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9f21561e5b8d9a74d1e1709c5e8ad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67a5a7eb32127274710d",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "9d06cc764a8a5c6c430e",
+    "url": "/static/js/2.3c7fac71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c7fac71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a5a7eb32127274710d",
+    "url": "/static/js/main.8f30b352.chunk.js"
+  },
+  {
+    "revision": "bf250259d9fd61dc7dbe",
+    "url": "/static/js/runtime-main.ecec8c93.js"
+  }
+]);
